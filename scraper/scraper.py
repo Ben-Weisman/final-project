a17412ad2986df63c1d2f3fd7fbee06b6062007c
TalFeldman <95241362+TalFeldman@users.noreply.github.com>
2022-04-19 14:47:37 +0300

    Add files via upload
    scraper server

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,9 +3,21 @@ from unicodedata import category
 import requests
 from bs4 import BeautifulSoup
 import json
-from utils.db import redis_string
+from flask import Flask
+from flask import request
 
-# from utils.validations import validateRecipe
+app = Flask(__name__)
+
+
+@app.route("/getUrl")
+def getUrl():
+    url = request.args['url']
+    return main(url)
+
+
+
+
+#from utils.validations import validateRecipe
 # url = "https://www.bbc.co.uk/food/recipes/healthier_coconut_38904"
 
 
@@ -33,29 +45,34 @@ def getJsonFromDB(url):
         with open("utils\scraperDB.json","r") as f:
             data = json.load(f)
             for site in data["webSitesUrl"]:
-                if url.find(site['url'])!=-1:
+                if url.find(site['url']) != -1:
                     return site
             return NULL
     except:
-        print("someting went wrong")
+        print("something went wrong")
     finally:
         f.close()
-def getTitle(soup,recipeIndexJson):
+
+
+def getTitle(soup, recipeIndexJson):
     title = soup.find_all(recipeIndexJson['title']['lookFor'],class_=recipeIndexJson['title']['className'])
     return title[0].text
 
-def getDescription(soup,recipeIndexJson):
+
+def getDescription(soup, recipeIndexJson):
     description = soup.find_all(recipeIndexJson['description']['lookFor'],class_=recipeIndexJson['description']['className'])
     return description[0].text
 
-def getIngredients(soup,recipeIndexJson):
+
+def getIngredients(soup, recipeIndexJson):
     ingredientsList = soup.find_all(recipeIndexJson['ingredients']['lookFor'],class_=recipeIndexJson['ingredients']['className'])
     ingredients = []
     for ingredientEl in ingredientsList:
         ingredients.append(ingredientEl.text)
     return ingredients
 
-def getMethod(soup,recipeIndexJson):
+
+def getMethod(soup, recipeIndexJson):
     methodList = soup.find_all(recipeIndexJson['method']['lookFor'],class_=recipeIndexJson['method']['className'])
     methods = []
     for method in methodList:
@@ -64,34 +81,32 @@ def getMethod(soup,recipeIndexJson):
 
 # def getImage(soup,recipeIndexJson):  /// for later
 
-def getCategory(soup,recipeIndexJson):
+def getCategory(soup, recipeIndexJson):
     categories = soup.find_all(recipeIndexJson['category']['lookFor'],class_=recipeIndexJson['category']['className'])
-    if category[0]!=[]:
-      return category[0].text
+    if category[0] != []:
+        return category[0].text
 
 
-
-def createRecipeJson(soup,recipeIndexJson):
+def createRecipeJson(soup, recipeIndexJson):
     try:
-        title = getTitle(soup,recipeIndexJson)
-        desctiption = getDescription(soup,recipeIndexJson)
-        ingredients = getIngredients(soup,recipeIndexJson)
-        method = getMethod(soup,recipeIndexJson)
+        title = getTitle(soup, recipeIndexJson)
+        description = getDescription(soup, recipeIndexJson)
+        ingredients = getIngredients(soup, recipeIndexJson)
+        method = getMethod(soup, recipeIndexJson)
         try:
-            category = getCategory(soup,recipeIndexJson)
+            category = getCategory(soup, recipeIndexJson)
         except:
             print("No category")
     # image = getImage(soup,recipeIndexJson)  /// for later
         dic = {}
-        dic["title"] =title 
-        dic["description"]= desctiption
-        dic["ingredients"]=ingredients
-        dic["method"]= method
+        dic["title"] = title
+        dic["description"] = description
+        dic["ingredients"] = ingredients
+        dic["method"] = method
         return json.dumps(dic)
+
     except:
         print("error in createRecipeJson")
-
-
 
 
 def main(url):
@@ -99,19 +114,20 @@ def main(url):
     content = res.text
     soup = BeautifulSoup(content, features="html.parser")
     try: 
-       recipeIndexJson =  getJsonFromDB(url)
+       recipeIndexJson = getJsonFromDB(url)
        if recipeIndexJson !=NULL:
         # print(recipeIndexJson)
-        recipeJson = createRecipeJson(soup,recipeIndexJson)
+        recipeJson = createRecipeJson(soup, recipeIndexJson)
+
         print(recipeJson) # test log
-        # validateRecipe(recipeJson)
+        return recipeJson
+       # validateRecipe(recipeJson)
     except:
         print("couldn't find json")
 
-      
-
 
 if __name__ == '__main__':
-    url = "https://www.bbc.co.uk/food/recipes/easter_brownies_39845"
-    # redis_string()
-    main(url)
+    #url = "https://www.bbc.co.uk/food/recipes/healthier_coconut_38904"
+    #url = getUrl()
+    #main(url)
+    app.run(debug=True)
