507004f98c24e920335cae7e85ebfb8e6c5f85a7
Guy-Zinger <guyz2310@gmail.com>
2022-07-27 20:36:40 +0300

    chrome extension react first commit

---
+++ chrome-extension-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from "react";
+import MainScreen from './components/Screens/MainScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <MainScreen />
+    </div>
+  );
+}
+
+export default App;
