95b2eae29670e93a8130eeb71ebb32b4b509a99b
Tal <tal.feldman.1991@gmail.com>
2022-08-16 18:08:21 +0300

    recieve json

--- chrome-extension-react/src/App.js
+++ chrome-extension-react/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from "react";
 import MainWindow from './components/mainWindow';
+import ChromeTabs from '@sinm/react-chrome-tabs/dist/chrome-tabs';
 
 
 
 function App() {
+
   return (
     <div className="App">
       <MainWindow />
