d7c3ddbde76690d088e636f0c4a4ffd2f64b3b7e
TalFeldman <95241362+TalFeldman@users.noreply.github.com>
2022-08-16 20:19:04 +0300

    Add files via upload

--- chrome-extension-react/src/components/showRecipe.js
+++ chrome-extension-react/src/components/showRecipe.js
@@ -1,31 +1,28 @@
 
 import { useState, useEffect } from "react";
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import {
-    Card,
-    CardActions,
-    CardContent,
-    CardHeader,
-    CardMedia,
-    Button,
-    Grid,
-    IconButton,
-    List,
-    ListItem,
-    Avatar,
-    ListItemText,
-    Typography
-  } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 import NewWindow from "react-new-window";
-import { EditText, EditTextarea } from 'react-edit-text';
 import SaveRecipe from './saveRecipe';
+import EditIngredients from './editIngredients'
+import EditInstructions from './editInstructions'
+import { EditText, EditTextarea } from 'react-edit-text';
 import 'react-edit-text/dist/index.css';
 
 
+import{
+    RecipeWrapper,
+    RecipeTitle,
+    SaveButton,
+    EditButton,
+    ParentDiv,
+    ChildDiv,
+    RecipeHeader,
+    Title,
+    Description
+} from "./Recipes.styles";
 
-function ShowRecipe({ recipe }) {
+
+export default function ShowRecipe({ recipe }) {
 
 
     const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
@@ -35,87 +32,83 @@ function ShowRecipe({ recipe }) {
     const [description, setDescription] = useState(recipe.recipe_description);
     const [ingredients, setIngredients] = useState(recipe.ingredients);
     const [instructions, setInstructions] = useState(recipe.recipe_instructions);
+    const image = recipe.image;
 
+    const [editIng, setEditIng] = useState(false);
+    const [editIns, setEditIns] = useState(false);
 
-    const changeRecipeName = ({ value }) => {
-        setRecipeName(value);
+    const openEditIng = () => {
+        setEditIng(!editIng)
+    }
+
+    const openEditIns = () => {
+        setEditIns(!editIns)
+    }
+
+    const changeTitle = ({ value}) => {
+        setRecipeName(value)
     };
 
-    const changeDescription = ({ value }) => {
-        setDescription(value);
+    const changeDescription = ({ value}) => {
+        setDescription(value)
     };
 
-    const changeIngredients = ({ value, previousValue }) => {
-        setIngredients(ingredients.map((item) => item === previousValue ? value : item
-        ));
-    };
 
-    const changeInstructions = ({ value, previousValue }) => {
-        setInstructions(instructions.map((item) => item === previousValue ? value : item
-        ));
-    };
 
 
     return (
-        <NewWindow>
-
-            <Card >
-                <CardHeader
-                    title={<Typography gutterBottom variant="h5" component="h2">
-                        <EditTextarea rows="1"
-                            defaultValue={recipe_name}
-                            style={{ fontSize: '16px' }}
-                            onSave={changeRecipeName} />
-                    </Typography>} />
-                <CardContent>
-                    <EditTextarea
+        <NewWindow title="Recipe Preview" center="screen">   
+            <RecipeWrapper> 
+                <RecipeHeader>Recipe Preview</RecipeHeader>         
+                <RecipeTitle>
+                     <EditText 
+                        defaultValue={recipe_name}
+                        onSave = {changeTitle}/>
+                </RecipeTitle>
+                <Description>
+                    {description? 
+                     <EditTextarea 
                         defaultValue={description}
-                        onSave={changeDescription} />
-                    <Grid item xs={12} md={6}>
-                        <Typography variant="h6" color="text.secondary">
-                            Ingredients:
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {ingredients.map(item => (
-                                <ListItem>
-                                    {<EditText
-                                        defaultValue={item}
-                                        onSave={changeIngredients} />}
-                                </ListItem>
+                        onSave = {changeDescription}
+                        rows= "event.target.rows"
+                        /> : ""}
+                </Description>               
+                <img src={image} alt="recpie img" width="300" height="auto" />               
+                <ParentDiv>
+                    <ChildDiv>
+                        <Title>Ingredients:</Title>
+                        <EditButton onClick={openEditIng}>Edit Ingredients</EditButton>
+                        {wait(1 * 1000) && editIng && 
+                        <EditIngredients ingredients={ingredients}  changeIng={ingredients=>setIngredients(ingredients)}>
+                            </EditIngredients>} 
+                        {ingredients.map(item => (
+                            <li>{item}</li>
+                        ))}                 
+                    </ChildDiv>
+                    <ChildDiv>
+                        <Title>Instructions:</Title>  
+                        <EditButton onClick={openEditIns}>Edit Instructions</EditButton>
+                        {wait(1 * 1000) && editIns && 
+                        <EditInstructions instructions={instructions} changeIns={instructions=>setInstructions(instructions)}>
+                            </EditInstructions>}                     
+                        {instructions.map(item => (
+                                <li>{item}</li>
                             ))}
-                        </Typography>
-                    </Grid>
-                    <Typography variant="h6" color="text.secondary">
-                        Instructions:
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        <List>
-                            {instructions.map(item => (
-                                <li>
-                                    {<EditTextarea
-                                        defaultValue={item}
-                                        onSave={changeInstructions} />}
-                                </li>
-                            ))}
-                        </List>
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="Medium" variant="contained" onClick={() => setSave(true)}>Save</Button>
+                    </ChildDiv>
+                </ParentDiv>              
+                <SaveButton onClick={() => setSave(true)}>Save Recipe</SaveButton>
                     {wait(1 * 1000) && save &&
-                        <SaveRecipe
-                            recipe_name={recipe_name}
-                            description={description}
-                            ingredients={ingredients}
-                            instructions={instructions}
-                        ></SaveRecipe>}
-                </CardActions>
-            </Card>
+                    <SaveRecipe
+                        recipe_name={recipe_name}
+                        description={description}
+                        ingredients={ingredients}
+                        instructions={instructions}
+                    />}
+            </RecipeWrapper>            
         </NewWindow>
     );
 }
 
-export default ShowRecipe;
 
 
 
