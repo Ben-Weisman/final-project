9a4d3300651299a960dfcaf1eecd5c43a04c0667
Ben-Weisman <54437197+Ben-Weisman@users.noreply.github.com>
2022-08-17 18:31:27 +0300

    Changed route methods with payload to post.

--- app/routes/recipesRoutes.js
+++ app/routes/recipesRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const recipesController = require('./../controllers/recipesController')
 
 
-router.get('/get-by-owner', recipesController.getAllRecipesByOwner); // done migrating
+router.post('/get-by-owner', recipesController.getAllRecipesByOwner); // done migrating
 
 router.get('/get-all', recipesController.getAll); // done migrating
 
-router.get('/cookbook',recipesController.getCookbook); // done migrating --> Gets: {email:}
+router.post('/cookbook',recipesController.getCookbook); // done migrating --> Gets: {email:}
 
 router.post('/add-new', recipesController.createNewRecipe); // done migrating --> // Gets: {name:,description:,category:,ingredients:,instructions:,ownerEmail:}
 
@@ -15,6 +15,6 @@ router.delete('/remove', recipesController.removeByID); // done migrating
 
 router.post('/add-existing-to-cookbook', recipesController.addExistingRecipeToCookbook);
 
-router.get('/get-by-name', recipesController.getByName);
+router.post('/get-by-name', recipesController.getByName);
 
 module.exports = router;
