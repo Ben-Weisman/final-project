27e1c71be25752c1842c06d4f46e5652f8069531
Tal <tal.feldman.1991@gmail.com>
2022-09-04 22:03:49 +0300

    working websites

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,8 +5,11 @@ from bs4 import BeautifulSoup
 import json
 from flask import Flask
 from flask import request
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/getUrl")
