ea0c0393872e8652b0b4d756c856f7c36474b91c
Tal <tal.feldman.1991@gmail.com>
2022-09-12 20:17:50 +0300

    server update

--- react-recipes-ui/src/components/Forms/SignUp/SignUp.js
+++ react-recipes-ui/src/components/Forms/SignUp/SignUp.js
@@ -14,6 +14,7 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Swal from "sweetalert2";
 
+
 function Copyright(props) {
   return (
     <Typography
